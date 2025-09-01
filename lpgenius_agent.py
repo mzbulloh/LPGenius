06fe57163cba6727ec0767283f4469b34c97bba7
Ahmad habibulloh <115066718+mzbulloh@users.noreply.github.com>
2025-09-02 06:25:22 +0700

    Update lpgenius_agent.py

--- lpgenius_agent.py
+++ lpgenius_agent.py
@@ -1,6 +1,6 @@
 from langchain.chat_models import AzureChatOpenAI
 
-OPENAI_API_KEY = "4QQWElITIFiMC9JMp9T6clC6jTRTpm7B9cLtF3vTzw0sOfmsi23FJQQJ99BEACYeBjFXJ3w3AAABACOGnPE1"
+OPENAI_API_KEY = "4xxx"
 OPENAI_API_BASE = "https://lpgenius.openai.azure.com/"
 OPENAI_API_VERSION = "2025-01-01-preview"
 
